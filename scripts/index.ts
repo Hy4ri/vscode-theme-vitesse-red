e6f673ddd06a7ae0cfc4a7de3564299009b0f864
hy4ri <hiari45@gmail.com>
2024-07-01 21:27:30 +0300

    aaaaaaaaaaa

--- scripts/index.ts
+++ scripts/index.ts
@@ -7,45 +7,10 @@ console.log('starting')
 fs.mkdir('./themes', { recursive: true })
   .then(() => Promise.all([
     fs.writeJSON(
-      './themes/vitesse-light.json',
-      getTheme({
-        color: 'light',
-        name: 'Vitesse Light',
-      }),
-      { spaces: 2 },
-    ),
-    fs.writeJSON(
-      './themes/vitesse-dark.json',
+      './themes/m57.json',
       getTheme({
         color: 'dark',
-        name: 'Vitesse Dark',
-      }),
-      { spaces: 2 },
-    ),
-    fs.writeJSON(
-      './themes/vitesse-black.json',
-      getTheme({
-        color: 'dark',
-        name: 'Vitesse Black',
-        black: true,
-      }),
-      { spaces: 2 },
-    ),
-    fs.writeJSON(
-      './themes/vitesse-light-soft.json',
-      getTheme({
-        color: 'light',
-        name: 'Vitesse Light Soft',
-        soft: true,
-      }),
-      { spaces: 2 },
-    ),
-    fs.writeJSON(
-      './themes/vitesse-dark-soft.json',
-      getTheme({
-        color: 'dark',
-        name: 'Vitesse Dark Soft',
-        soft: true,
+        name: 'm57',
       }),
       { spaces: 2 },
     ),
@@ -54,27 +19,10 @@ fs.mkdir('./themes', { recursive: true })
 fs.mkdir('./extra', { recursive: true })
   .then(() => Promise.all([
     fs.writeJSON(
-      './extra/xterm-vitesse-light.json',
-      getXtermTheme({
-        color: 'light',
-        name: 'Vitesse Light',
-      }),
-      { spaces: 2 },
-    ),
-    fs.writeJSON(
-      './extra/xterm-vitesse-dark.json',
+      './extra/xterm-m57-red.json',
       getXtermTheme({
         color: 'dark',
-        name: 'Vitesse Dark',
-      }),
-      { spaces: 2 },
-    ),
-    fs.writeJSON(
-      './extra/xterm-vitesse-black.json',
-      getXtermTheme({
-        color: 'dark',
-        name: 'Vitesse Black',
-        black: true,
+        name: 'M57',
       }),
       { spaces: 2 },
     ),
